5252a04da25db57298cd6249bc6fa05ae7eec9ba
eddiechilton <eddienewman@protonmail.com>
2024-04-04 15:01:31 -0400

    added route for quiz data

---
+++ backend/fakeDb.js
@@ -0,0 +1,61 @@
+var fakeDb = {
+    quizData: [
+      {
+          quizId: 1,
+        metadata: {
+          quizName: "Math quiz 1",
+          quizAuthor: "John Doe",
+          quizInstructor: "Jane Doe",
+        },
+        questions: [
+          {
+            number: 1,
+            header: "Add two numbers",
+            body: "x = 2 + 3",
+            answers: ["1", "4", "5", "17"],
+          },
+          {
+            number: 2,
+            header: "Subract two numbers",
+            body: "y = 10 - 3",
+            answers: ["2", "3", "0", "7"],
+          },
+        ],
+        correctAnswers: ["5","7"]
+      },
+      {
+        quizId: 2,
+        metadata: {
+          quizName: "reading quiz 2",
+          quizAuthor: "Janet Doe",
+          quizInstructor: "James Doe",
+        },
+        questions: [
+          {
+            number: 1,
+            header: "Nouns",
+            body: "Which is a noun?",
+            answers: ["frolicking", "dog", "it", "drive"],
+          },
+          {
+            number: 2,
+            header: "Verbs",
+            body: "Which is a verb?",
+            answers: ["frolicking", "dog", "it", "drive"],
+          },
+        ],
+        correctAnswers: ["dog","drive"]
+      },
+    ],
+    attemptData: [
+      {
+        attemptHash: "asdfasdfasdf",
+        attemptName: "eddie",
+        lastCompleted: 0,
+        completedAnswers: [],
+        finished: false,
+      },
+    ],
+  };
+
+  module.exports = fakeDb
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
+var fakeDb = require('./fakeDb')
 
-app.get('/', (req, res) => {
-  res.send('Successful response.');
+console.log(fakeDb)
+
+app.get("/", (req, res) => {
+  res.send("Successful response.");
 });
 
-app.get('/quiz/:quizId', (req, res) => {
-  res.send('Successful response.'+req.params.quizId);
+app.get("/quiz/:quizId", (req, res) => {
+  const quizData = fakeDb.quizData.find(({ quizId }) => quizId == req.params.quizId);
+  const { correctAnswers, ...quizDataMinusAnswers } = quizData;
+  res.send(quizDataMinusAnswers);
 });
 
-app.listen(3000, () => console.log('listening on port 3000.'));
+app.listen(3000, () => console.log("listening on port 3000."));
